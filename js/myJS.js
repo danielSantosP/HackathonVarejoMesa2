de25fbb072e5a0af58889b52751f9dc28e24e389
danielSantosP <danielsantospatrocinio@gmail.com>
2018-06-22 02:35:57 -0300

    FinalizandoForm
    Criação de javascript, finalizado formulário e css

---
+++ js/myJS.js
@@ -0,0 +1,10 @@
+
+
+
+function enviarDados(){
+    var nome = document.getElementById("nomeInput");
+
+    if (nome.value != ""){
+        alert('Obrigado senhor' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    } 
+};
